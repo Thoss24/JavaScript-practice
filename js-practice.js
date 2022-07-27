b64b873d1d10d0dd7a7cb19485b0c283e2c5396e
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-27 16:09:49 +0100

    Time and date added

--- js-practice.js
+++ js-practice.js
@@ -1,6 +1,5 @@
-function getComputerChoice() {
-    let myArray = ["Rock", "Paper", "Scissors"];
-    let myValue = myArray[Math.floor(Math.random() * myArray.length)];
-    console.log(myValue)
+function getTimeAndDate() {
+    let today = new Date();
+    console.log(today);
 }
-getComputerChoice();
+getTimeAndDate();
