3c3058fe3694e023d9fe522d0b45fef5f0843764
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-27 16:22:15 +0100

    Show current date dd/mm/yy format added

--- js-practice.js
+++ js-practice.js
@@ -5,6 +5,13 @@ function getTimeAndDate() {
 }
 getTimeAndDate();
 
-// How to print contents of current window
-console.log(window.print());
+// How to print current window
+window.print();
 
+// How to get date dd/mm/yy format. Remember getMonth is 0 indexed so need + 1.
+function getDateOnly() {
+    let date = new Date();
+    let newDate = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+    console.log(newDate);
+}
+getDateOnly();
