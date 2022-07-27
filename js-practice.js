48357b9bf483ce879549210768092a08e66da46a
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-27 16:12:45 +0100

    How to print content of window added

--- js-practice.js
+++ js-practice.js
@@ -1,5 +1,10 @@
+// How to get current time and date
 function getTimeAndDate() {
     let today = new Date();
     console.log(today);
 }
 getTimeAndDate();
+
+// How to print contents of current window
+console.log(window.print());
+
