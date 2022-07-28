e45d6eca85ced5ec2e04f3167cdf66f1db343edd
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-28 13:34:14 +0100

    pop(), shift(), unshift(), push() method practice

--- array-methods.js
+++ array-methods.js
@@ -5,3 +5,26 @@ console.log(fruit.toString())
 // join() joins all array elements into a string but the separator can be defined
 const veg = new Array("potato", "cabbage", "turnip", "broccoli");
 console.log(veg.join("*"))
+
+// pop(), push(), shift(), unshift()
+const animals = ["cat", "dog", "fish", "cow"];
+let lastAnimal = animals.pop();
+console.log(lastAnimal); // remove last array value
+
+animals.push("sheep"); // push new value to end of array
+console.log(animals);
+
+console.log(animals.shift()); // remove first array value
+
+animals.unshift("panda"); // add new value to start of array
+console.log(animals)
+
+// .length can append new element to end of array. Original array retains old value with new value added to the end.
+animals[animals.length] = "chicken";
+console.log(animals);
+
+// delete() will completely delete an element from an array
+delete(animals[4]);
+console.log(animals);  // "chicken" has now been deleted
+
+//
