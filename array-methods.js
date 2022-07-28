bba9b7f910474194109ef6e4404ef6407e6be70c
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-28 13:10:31 +0100

    join() and toString() method practice

---
+++ array-methods.js
@@ -0,0 +1,7 @@
+// toString() expected output a string of comma separated array values
+const fruit = ["banana", "apple", "orange", "pear"];
+console.log(fruit.toString())
+
+// join() joins all array elements into a string but the separator can be defined
+const veg = new Array("potato", "cabbage", "turnip", "broccoli");
+console.log(veg.join("*"))
