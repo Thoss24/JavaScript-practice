ffaa3ae7c2dbaae780d55c2cf29d972e64b231f0
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-05 12:33:03 +0100

    h3 element with blue text added

--- qs-javascript.js
+++ qs-javascript.js
@@ -1,3 +1,10 @@
+// <p> element added, practicing JS, and DOM methods.
 let paragraphOne = document.createElement('p');
 paragraphOne.textContent = 'Hey I\'m red!';
 document.querySelector('body').appendChild(paragraphOne)
+
+// <h3> element with blue text.
+let blueHeaderOne = document.createElement('h3');
+blueHeaderOne.textContent = 'I\'m a blue h3!';
+blueHeaderOne.style.color = "blue";
+document.querySelector('body').appendChild(blueHeaderOne);
