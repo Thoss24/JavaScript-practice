e608a498534e16a8aea08ee2aa5a6f8f9d510a43
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-09 17:41:00 +0100

    functional ('click', run code snippet)

--- qs-javascript.js
+++ qs-javascript.js
@@ -74,12 +74,40 @@ divs.forEach(div => div.addEventListener('click', logText, {  // An event to exe
 }));
 
 // rps ui code
-let buttonTwo = document.querySelectorAll('button');
-buttonTwo.forEach((item) => item.addEventListener('click', function(e) {
-  
-  const input = this.textContent;
-  
-  const cInput = 
+// selector
+document.getElementById('rock').onclick = user;
+document.getElementById('paper').onclick = user;
+document.getElementById('scissors').onclick = user;
 
-  
-}, ))
+//function to compare player selection and computer selection
+function user() {
+    let playerSelection = this.id;
+
+    let computerChoice = ["rock", "paper", "scissors"];
+    let computerSelection = computerChoice[Math.floor(Math.random() * 3)]
+
+
+// Function to play a round of "Rock", "Paper", "Scissors". 
+function playRound(choice1, choice2) { 
+  if (
+    (choice1 == 'rock' && choice2 == 'scissors') ||
+    (choice1 == 'paper' && choice2 == 'rock') ||
+    (choice1 == 'scissors' && choice2 == 'paper')
+    ) { alert(`You win! ${choice1} beats ${choice2}.`);
+        playerScore++;
+    }
+    
+    else if(
+    (choice1 == 'rock' && choice2 == 'paper') ||
+    (choice1 == 'paper' && choice2 == 'scissors') ||
+    (choice1 == 'scissors' && choice2 == 'rock')
+    ) { alert(`You lose, ${choice2} beats ${choice1}.`);
+        computerScore++;
+    } 
+
+    else if (choice1 == choice2){
+        alert(`It's a tie, ${choice1} and ${choice2} are equal.`);
+    }
+}  
+console.log(playRound(playerSelection, computerSelection))
+}
