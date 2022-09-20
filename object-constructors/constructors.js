131f597d948b8fa5693bcd269a59e5277f444311
Thomas Fogarty <t.fogarty24@outlook.com>
2022-09-20 16:11:46 +0100

    inheritance and prototypes

--- object-constructors/constructors.js
+++ object-constructors/constructors.js
@@ -12,7 +12,10 @@ const hobbit = new Book('The Hobbit', 'J.R.R Tolkien', '280 pages', 'have read.'
 console.log(hobbit.info())
 
 console.log(hobbit.constructor)  // The new instance's prototype (hobbit) is assigned the prototype from the Book() constructor. hobbit is basically inheriting the prototype from the Book() constructor.
-                                 
+            
+
+
+
 // In JavaScript you can create a function and add properties and methods on to that function's prototype property, and all instances of the function will inherit the function's properties and methods.
 
 function Plant() {
@@ -20,24 +23,31 @@ function Plant() {
     this.isOrganic = true
 }
 
-Plant.prototype.showNameAndColor = function() {
+Plant.prototype.showNameAndColor = function() {  // The showNameAndColor function is added to the Plant prototype property
     console.log("I am a " + this.name + " and my color is " + this.color);
 }
 
-Plant.prototype.amIOrganic = function() {
+Plant.prototype.amIOrganic = function() {  //The amIOrganic function is added to the Plant prototype property
     if (this.isOrganic)
     console.log("I am organic");
 }
 
-function Fruit (fruitName, fruitColor) {
+function Fruit (fruitName, fruitColor) { // A new constructor (Fruit) is made
     this.name = fruitName;
     this.color = fruitColor;
 }
 
-Fruit.prototype = new Plant ()
+Fruit.prototype = new Plant () // The Fruit() prototype is set to equal the Plant() constructor. Thus, Fruit() inherits all of Plant.prototype methods and properties.
 
-let aBanana = new Fruit("Banana", "Yellow")
+let aBanana = new Fruit("Banana", "Yellow")  // Creates a new object (aBanana), with the Fruit() constructor.
 
-console.log(aBanana.name) // Banana
+console.log(aBanana.name) // aBanana uses the name property from the aBanana object prototype, which is Fruit.prototype
 
-console.log(aBanana.showNameAndColor())
+console.log(aBanana.showNameAndColor())  // The aBanana object inherits all the properties and methods from both the plant and fruit functions
+
+// Even though the showNameAndColor method was inherited by the aBanana object, even though it was defined all the way up on the prototype chain
+// Any object that used the Fruit() constructor will inherit all of the Fruit.prototype properties and methods, in addition to inheriting all the properties and methods from Fruit's() prototype, which is the Plant.prototype.
+
+// This is a clear example of how inheritance is implemented in JavaScript and the crucial role the prototype chain has in the process.
+
+
