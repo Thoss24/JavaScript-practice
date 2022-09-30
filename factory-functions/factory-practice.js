56aeb56e1c1fd8e42de113ddc8bf866b74d37ff5
Thomas Fogarty <t.fogarty24@outlook.com>
2022-09-30 13:47:48 +0100

    concatenative inheritance

--- factory-functions/factory-practice.js
+++ factory-functions/factory-practice.js
@@ -15,10 +15,28 @@ jeff.sayName(); // "My name is Jeff"
 jeff.doSomethingNerdy(); // "nerd stuff"
 
 
-// Object.assign() method
+// Object.assign() method to copy the properties from one object to another
 const target = { a: 1, b: 2};
 const source = { c: 3, d: 4};
 
 const returnedObject = Object.assign(target, source)
 
 console.log(target) // Expected output Object { a: 1, b: 2, c: 2, d: 4}
+
+
+// Delegation / Differential Inheritance using Object.create()
+const proto = {
+    hello () {
+        return `Hello, my name is ${this.name}`;
+    }
+};
+
+const greeter = (name) => Object.assign(Object.create(proto), {  // Object.create() creates a new object, using an existing object as the prototype of the newly created object
+    name
+});
+
+const george = greeter('george');
+
+const msg = george.hello();
+
+console.log(msg);
