2d266d9e65306bce1222440bdad0b9656d2e01f7
Thomas Fogarty <t.fogarty24@outlook.com>
2022-09-30 12:16:31 +0100

    object.assign() method practice

--- factory-functions/factory-practice.js
+++ factory-functions/factory-practice.js
@@ -13,3 +13,12 @@ const jeff = Nerd('Jeff');
 
 jeff.sayName(); // "My name is Jeff"
 jeff.doSomethingNerdy(); // "nerd stuff"
+
+
+// Object.assign() method
+const target = { a: 1, b: 2};
+const source = { c: 3, d: 4};
+
+const returnedObject = Object.assign(target, source)
+
+console.log(target) // Expected output Object { a: 1, b: 2, c: 2, d: 4}
