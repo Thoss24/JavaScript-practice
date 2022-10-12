03efb3c88958b4de09030846d68044e586525bdf
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-12 11:46:00 +0100

    another getter/setter example

--- getters-setters/getter-setter.js
+++ getters-setters/getter-setter.js
@@ -15,4 +15,28 @@ console.log(user.fullName)
 console.log(user.name)
 
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+// another example of using getters and setters
+   
+const person = {
+    name: "henry",
+    surname: "help",
+    language: "eng",
+    get lang() {
+        return this.language
+    }
+};
+
+console.log(person.lang)
+
+const person2 = {
+    name: "john",
+    surname: "doe",
+    language: "",
+    set lang(lang) {
+        this.language = lang;
+    }
+};
+
+person2.lang = "swe";
+
+console.log(person2.language)
