1e023db9e333edcb1f1c423b5acd2cc92697d279
Thomas Fogarty <t.fogarty24@outlook.com>
2023-01-27 14:33:14 +0000

    replace with alphabet position

--- coding-challenges/challenges.js
+++ coding-challenges/challenges.js
@@ -4,12 +4,24 @@ class Exercises {
         val = 153
         let num = val.toString().split('').map(Number)
         let armNumber = num.map(a => {return a**3}).reduce((a, b) => a + b);
-        
+
         if (armNumber == val) {
             console.log("True")
         }
         else console.log("false")
     }
+
+    static replaceWithAlphabet(text) {
+        text = "hello"
+        let result = ""
+        for (let i = 0; i < text.length; i++){
+            let char = text.toUpperCase().charCodeAt(i) 
+            // charCodeAt returns integer between 0 and 65535 representing UTF-16 at the given index
+            if (char > 64 && char < 91) result += (char - 64) + " ";
+        }
+
+        console.log(result.slice(0, result.length - 1));
+    }
 };
 
-Exercises.armstrongNumber()
+Exercises.replaceWithAlphabet()
